69094ee65982088ed074ee6930a1780fe8935ddd
mau89 <43510463+mau89@users.noreply.github.com>
2019-04-25 00:57:41 +0500

    Реализовал столкновение кораблей и попадание вражеского корабля. не успел реализовать попадание во вражеский корабль

---
+++ 1_lesson/core/src/ru/geekbrains/screen/GameScreen.java
@@ -0,0 +1,173 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
+import ru.geekbrains.pool.EnemyPool;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Bullet;
+import ru.geekbrains.sprite.Enemy;
+import ru.geekbrains.sprite.MainShip;
+import ru.geekbrains.sprite.Star;
+import ru.geekbrains.utils.EnemyGenerator;
+
+public class GameScreen extends BaseScreen {
+
+    private Texture bg;
+    private Background background;
+    private TextureAtlas atlas;
+    private Star starList[];
+
+    private MainShip mainShip;
+
+    private BulletPool bulletPool;
+    private EnemyPool enemyPool;
+
+    private Music music;
+    private Sound laserSound;
+    private Sound bulletSound;
+
+    private EnemyGenerator enemyGenerator;
+
+    @Override
+    public void show() {
+        super.show();
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        music.setLooping(true);
+        music.play();
+        laserSound = Gdx.audio.newSound(Gdx.files.internal("sounds/laser.wav"));
+        bulletSound = Gdx.audio.newSound(Gdx.files.internal("sounds/bullet.wav"));
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+        starList = new Star[64];
+        for (int i = 0; i < starList.length; i++) {
+            starList[i] = new Star(atlas);
+        }
+        bulletPool = new BulletPool();
+        mainShip = new MainShip(atlas, bulletPool, laserSound);
+        enemyPool = new EnemyPool(bulletPool, bulletSound, worldBounds, mainShip);
+        enemyGenerator = new EnemyGenerator(atlas, enemyPool, worldBounds);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        checkCollisions();
+        freeAllDestroyedSprites();
+        draw();
+    }
+
+    private void update(float delta) {
+        for (Star star : starList) {
+            star.update(delta);
+        }
+        mainShip.update(delta);
+        bulletPool.updateActiveSprites(delta);
+        enemyPool.updateActiveSprites(delta);
+        enemyGenerator.generate(delta);
+    }
+
+    private void checkCollisions() {
+
+        for (Enemy aaa : enemyPool.getActiveObjects()) {
+
+            if (!aaa.isOutside(mainShip)) {
+
+                mainShip.destroy();
+                aaa.destroy();
+
+            }
+        }
+
+        for (int i = 0; i < bulletPool.getActiveObjects().size(); i++) {
+
+            if (!(bulletPool.getActiveObjects().get(i).getOwner() == mainShip)) {
+                Bullet aaaaa = bulletPool.getActiveObjects().get(i);
+
+                if (!aaaaa.isOutside(mainShip)) {
+
+                    mainShip.destroy();
+                    aaaaa.destroy();
+                }
+
+            }
+        }
+
+    }
+
+    private void freeAllDestroyedSprites() {
+        bulletPool.freeAllDestroyedActiveSprites();
+        enemyPool.freeAllDestroyedActiveSprites();
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        for (Star star : starList) {
+            star.draw(batch);
+        }
+        if (!mainShip.isDestroyed()) {
+            mainShip.draw(batch);
+        }
+        bulletPool.drawActiveSprites(batch);
+        enemyPool.drawActiveSprites(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : starList) {
+            star.resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+        atlas.dispose();
+        bulletPool.dispose();
+        enemyPool.dispose();
+        music.dispose();
+        laserSound.dispose();
+        bulletSound.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        mainShip.touchDown(touch, pointer);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        mainShip.touchUp(touch, pointer);
+        return false;
+    }
+}
---
+++ 1_lesson/core/src/ru/geekbrains/sprite/MainShip.java
@@ -0,0 +1,151 @@
+package ru.geekbrains.sprite;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.Ship;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
+
+public class MainShip extends Ship {
+
+    private static final int INVALID_POINTER = -1;
+
+    private boolean pressedRight;
+    private boolean pressedLeft;
+
+    private int rightPointer = INVALID_POINTER;
+    private int leftPointer = INVALID_POINTER;
+
+    public MainShip(TextureAtlas atlas, BulletPool bulletPool, Sound shootSound) {
+        super(atlas.findRegion("main_ship"), 1, 2, 2);
+        this.bulletPool = bulletPool;
+        this.bulletRegion = atlas.findRegion("bulletMainShip");
+        this.shootSound = shootSound;
+        setHeightProportion(0.15f);
+        this.reloadInterval = 0.2f;
+        this.bulletV.set(0f, 0.5f);
+        this.bulletHeight = 0.015f;
+        this.damage = 1;
+        this.v0.set(0.5f, 0);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        setBottom(worldBounds.getBottom() + 0.05f);
+    }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+        reloadTimer += delta;
+        if (reloadTimer >= reloadInterval) {
+            reloadTimer = 0f;
+            shoot();
+        }
+        if (getRight() > worldBounds.getRight()) {
+            setRight(worldBounds.getRight());
+            stop();
+        }
+        if (getLeft() < worldBounds.getLeft()) {
+            setLeft(worldBounds.getLeft());
+            stop();
+        }
+    }
+
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.A:
+            case Input.Keys.LEFT:
+                pressedLeft = true;
+                moveLeft();
+                break;
+            case Input.Keys.D:
+            case Input.Keys.RIGHT:
+                pressedRight = true;
+                moveRight();
+                break;
+            case Input.Keys.UP:
+                shoot();
+                break;
+        }
+        return false;
+    }
+
+    public boolean keyUp(int keycode) {
+        switch (keycode) {
+            case Input.Keys.A:
+            case Input.Keys.LEFT:
+                pressedLeft = false;
+                if (pressedRight) {
+                    moveRight();
+                } else {
+                    stop();
+                }
+                break;
+            case Input.Keys.D:
+            case Input.Keys.RIGHT:
+                pressedRight = false;
+                if (pressedLeft) {
+                    moveLeft();
+                } else {
+                    stop();
+                }
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        if (touch.x < worldBounds.pos.x) {
+            if (leftPointer != INVALID_POINTER) {
+                return false;
+            }
+            leftPointer = pointer;
+            moveLeft();
+        } else {
+            if (rightPointer != INVALID_POINTER) {
+                return false;
+            }
+            rightPointer = pointer;
+            moveRight();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        if (pointer == leftPointer) {
+            leftPointer = INVALID_POINTER;
+            if (rightPointer != INVALID_POINTER) {
+                moveRight();
+            } else {
+                stop();
+            }
+        } else if (pointer == rightPointer) {
+            rightPointer = INVALID_POINTER;
+            if (leftPointer != INVALID_POINTER) {
+                moveLeft();
+            } else {
+                stop();
+            }
+        }
+        return false;
+    }
+
+    private void moveRight() {
+        v.set(v0);
+    }
+
+    private void moveLeft() {
+        v.set(v0).rotate(180);
+    }
+
+    private void stop() {
+        v.setZero();
+    }
+}
